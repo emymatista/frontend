3c2e6ea6e1cf040dbadadcdad31b44acab1ef431
emymatista <emymatista05@gmail.com>
2024-06-23 21:39:31 -0400

    travis added

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,4 @@
+
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
@@ -12,5 +13,5 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
 })
